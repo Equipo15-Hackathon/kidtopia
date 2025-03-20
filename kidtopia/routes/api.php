a7cc5da333e49fe18a3fda4b8af59306bc41d96f
Miguel Ángel García López <mangel111111111@gmail.com>
2025-03-20 12:07:59 +0100

    feat: add API routes for Product, Category, Brand and AgeRange

--- kidtopia/routes/api.php
+++ kidtopia/routes/api.php
@@ -2,7 +2,35 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\BrandController;
+use App\Http\Controllers\Api\AgeRangeController;
 
-Route::get('/user', function (Request $request) {
+/* Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+})->middleware('auth:sanctum'); */
+
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::post('/categories', [CategoryController::class, 'store']);
+Route::get('/categories/{id}', [CategoryController::class, 'show']);
+Route::put('/categories/{id}', [CategoryController::class, 'update']);
+Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+
+Route::get('/brands', [BrandController::class, 'index']);
+Route::post('/brands', [BrandController::class, 'store']);
+Route::get('/brands/{id}', [BrandController::class, 'show']);
+Route::put('/brands/{id}', [BrandController::class, 'update']);
+Route::delete('/brands/{id}', [BrandController::class, 'destroy']);
+
+Route::get('/ageRanges', [AgeRangeController::class, 'index']);
+Route::post('/ageRanges', [AgeRangeController::class, 'store']);
+Route::get('/ageRanges/{id}', [AgeRangeController::class, 'show']);
+Route::put('/ageRanges/{id}', [AgeRangeController::class, 'update']);
+Route::delete('/ageRanges/{id}', [AgeRangeController::class, 'destroy']);
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::put('/products/{id}', [ProductController::class, 'update']);
+Route::delete('/products/{id}', [ProductController::class, 'destroy']);
